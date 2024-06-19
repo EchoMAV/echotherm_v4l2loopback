a822ceb362ea745f37aa6f88ea2fcc3350b3665d
Thomas Lyons <tlyons@echomav.com>
2024-06-19 15:27:10 -0400

    working on autostart script

--- seekcamera_capture_autostart.sh
+++ seekcamera_capture_autostart.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-# check if the process is running
+# check if a seek thermal camera is plugged in. Their vendor ID is 289d.
+if [ "$(lsusb | grep -c "289d:")" -ge 1 ]; then
+	# check if the process is running
 	case "$(pidof seekcamera_capture | wc -w)" in
 		0) # restart the process
 			seekcamera_capture &
@@ -11,3 +13,4 @@
 			kill -9 $(pidof seekcamera_capture | awk '{print $1}')
 	    		;;
 	esac
+fi
