eaa2af99950654aa8f241b8f6adc2c0b60f737f6
Thomas Lyons <tlyons@echomav.com>
2024-06-28 08:52:53 -0400

    cleaning up

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,12 @@
 #include "SeekCameraLoopHandler.h"
 #include "SeekCamera.h"
-
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/program_options.hpp>
 #include <sys/inotify.h>
-// C includes
 #include <csignal>
-
-
-
-
 
 SeekCameraLoopHandler g_loopHandler;
 
@@ -24,30 +18,24 @@ void sighandler(int signum)
 	g_loopHandler.stop();
 }
 
-
-
 void writeConfig(::std::filesystem::path const& path, ::std::unordered_map<::std::string, SeekCamera> & cameraMap)
 {
 	boost::property_tree::ptree properties;
 	boost::property_tree::ptree cameraArray;
-	properties.put("default_color_palette", SEEKCAMERA_COLOR_PALETTE_SPECTRA);
+	properties.put("default_color_palette", (int)SEEKCAMERA_COLOR_PALETTE_SPECTRA);
+	properties.put("default_shutter_mode", (int)SEEKCAMERA_SHUTTER_MODE_AUTO);
 	for(auto const& [cid, camera] : cameraMap)
 	{
 		boost::property_tree::ptree cameraProps;
 		cameraProps.put("cid", cid);
 		cameraProps.put("device_path", camera.getDevicePath());
 		cameraProps.put("format", camera.getFormat());
-		//cameraProps.put("color_palette", camera.getColorPalette());
 		cameraArray.push_back(::std::make_pair("", cameraProps));
 	}
 	properties.add_child("camera_array", cameraArray);
-
 	::std::ofstream outputStream(path);
 	boost::property_tree::write_json(outputStream, properties, true);
-
 }
-
-
 
 int readConfig(std::filesystem::path const& configFilePath, ::std::unordered_map<::std::string, SeekCamera> & cameraMap)
 {
@@ -58,16 +46,12 @@ int readConfig(std::filesystem::path const& configFilePath, ::std::unordered_map
 		boost::property_tree::ptree properties;
 		try
 		{
-			
 			::std::ifstream inputStream(configFilePath);
 			boost::property_tree::read_json(inputStream,properties);
-
 			seekcamera_color_palette_t defaultColorPallete = (seekcamera_color_palette_t)properties.get<int>("default_color_palette");
 			seekcamera_shutter_mode_t defaultShutterMode = (seekcamera_shutter_mode_t)properties.get<int>("default_shutter_mode");
-
 			g_loopHandler.setDefaultColorPalette(defaultColorPallete);
 			g_loopHandler.setDefaultShutterMode(defaultShutterMode);
-
 			auto cameraArrayOpt = properties.get_child_optional("camera_array");
 			if(cameraArrayOpt.is_initialized())
 			{
@@ -93,7 +77,6 @@ int readConfig(std::filesystem::path const& configFilePath, ::std::unordered_map
 	return returnCode;
 }
 
-
 int main(int argc, char* argv[])
 {
 	signal(SIGINT, sighandler);
@@ -105,8 +88,6 @@ int main(int argc, char* argv[])
 	boost::program_options::variables_map vm;
 	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
 	boost::program_options::notify(vm);
-
-	
 	std::filesystem::path configFilePath{};
 	std::unordered_map<::std::string, SeekCamera> cameraMap{};
 	bool proceed=true;
@@ -121,8 +102,6 @@ int main(int argc, char* argv[])
 		{
 			configFilePath = vm["configFile"].as<std::filesystem::path>();
 			std::cout<<"configFile = "<<configFilePath<<std::endl;
-
-
 			//SeekCamera seekCamera("/dev/video0", SEEKCAMERA_FRAME_FORMAT_COLOR_YUY2, SEEKCAMERA_COLOR_PALETTE_WHITE_HOT);
 			//cameraMap.emplace("E452AFB41114", ::std::move(seekCamera)).first;
 			//std::cout<<"writing to config file "<<std::filesystem::absolute(configFilePath)<<std::endl;
@@ -133,14 +112,8 @@ int main(int argc, char* argv[])
 			std::cout<<"no options found"<<std::endl;
 		}
 	}
-
-
-
-
-	
 	int notifyFileDescriptor = inotify_init();
 	int notifyWatchDescriptor = -1;
-	
 	if(!configFilePath.empty())
 	{
 		if(notifyFileDescriptor < 0)
@@ -154,12 +127,11 @@ int main(int argc, char* argv[])
 			{
 				std::cerr<<"inotify_add_watch on file '"<<configFilePath<<"' failed: "<<strerror(errno)<<std::endl;
 			}
-			
 		}
 	}
-
 	if(notifyFileDescriptor >=0 && notifyWatchDescriptor>=0)
 	{
+		//watch a config file and reload when it changes
 		for(;;)
 		{
 			if(readConfig(configFilePath, cameraMap))
@@ -173,7 +145,7 @@ int main(int argc, char* argv[])
 			}
 			char p_buffer[sizeof(inotify_event) + 16];
 			// wait until the configuration file is modified
-			ssize_t length=read(notifyFileDescriptor, p_buffer, sizeof(inotify_event) + 16);
+			auto const length=read(notifyFileDescriptor, p_buffer, sizeof(inotify_event) + 16);
 			if(length<0)
 			{
 				std::cerr<<"read failed: "<<strerror(errno)<<std::endl;
@@ -185,6 +157,7 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
+		//no config file, just press a key to exit
 		auto status = g_loopHandler.start(::std::move(cameraMap));
 		if (status == SEEKCAMERA_SUCCESS)
 		{
@@ -203,60 +176,3 @@ int main(int argc, char* argv[])
 	(void)close( notifyFileDescriptor );
 	return 0;
 }
-#if 0
-int main_(int argc, char* argv[])
-{
-
-
-
-	std::unordered_map<::std::string, SeekCamera> cameraMap{};
-	boost::program_options::options_description desc("Allowed options");
-	desc.add_options()
-		("help", "produce help message")
-		("configFile", boost::program_options::value<std::filesystem::path>(), "choose the config file path");
-	boost::program_options::variables_map vm;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-	boost::program_options::notify(vm);
-
-	if(vm.count("help"))
-	{
-		std::cout<<desc<<::std::endl;
-	}
-	else if(vm.count("configFile"))
-	{
-		//TODO read in a map of cameras and devices, probably from JSON
-		std::filesystem::path configFilePath = vm["configFile"].as<std::filesystem::path>();
-		std::cout<<"configFile = "<<configFilePath<<std::endl;
-		if(readConfig(configFilePath, cameraMap))
-		{
-			cameraMap.clear();
-			//SeekCamera seekCamera("/dev/video0", SEEKCAMERA_FRAME_FORMAT_COLOR_YUY2, SEEKCAMERA_COLOR_PALETTE_WHITE_HOT);
-			//cameraMap.emplace("E452AFB41114", ::std::move(seekCamera)).first;
-			//std::cout<<"writing to config file "<<std::filesystem::absolute(configFilePath)<<std::endl;
-			//writeConfig(configFilePath, cameraMap);
-		}
-	}
-	else
-	{
-		std::cout<<"no options found"<<std::endl;
-	}
-
-	
-	signal(SIGINT, sighandler);
-	signal(SIGTERM, sighandler);
-	auto status = g_loopHandler.start(::std::move(cameraMap));
-	if (status == SEEKCAMERA_SUCCESS)
-	{
-		std::cout << "press enter to quit" << ::std::endl;
-		for (;;)
-		{
-			int c = getchar();
-			if (c == '\n')
-			{
-				break;
-			}
-		}
-	}
-	return 0;
-}
-#endif
